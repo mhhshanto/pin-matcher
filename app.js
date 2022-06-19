23bfb0270213351c6590a9150ca9919cd5a670d4
shanto <hhassibul@gmail.com>
2022-06-19 16:51:43 +0600

    dgweu

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ function getPin(){
 function generatePin(){
   const newPin = getPin()
   const pintu = parseFloat(newPin)
-  console.log(pintu);
   document.getElementById('display-pin').value = pintu
 }
 document.getElementById('key-pad').addEventListener('click',function(a){
@@ -28,3 +27,34 @@ if(isNaN(number)){
 }
 
 })
+function verifyPin(){
+  var pin = document.getElementById('display-pin').value
+  var pin2 = document.getElementById('num-input').value
+  if(pin === pin2){
+    document.getElementById('success').style.display='block'
+    document.getElementById('failed').style.display='none'
+    document.getElementById('tryy').style.display='none'
+    
+  }else{
+    document.getElementById('success').style.display='none'
+    document.getElementById('failed').style.display='block'
+    var failCheck = document.getElementById('fail-count').innerHTML;
+    document.getElementById('fail-count').innerHTML= failCheck + '1';
+    if(failCheck === '1'){
+      document.getElementById('try-left').innerHTML = 2
+      console.log('1');
+    }
+    else if(failCheck === '11'){
+      document.getElementById('try-left').innerHTML = 1
+      console.log('2');
+    }
+    else if(failCheck === '111'){
+      document.getElementById('try-left').innerHTML = 'no';
+      console.log('3');
+      document.getElementById("btn").disabled = true;
+
+    }
+    console.log(failCheck);
+  }
+  
+}
